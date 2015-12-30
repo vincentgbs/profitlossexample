fd34ff35e89ee69fba4ce4aca4e332e79d55de28
vincent hu <vincent.gbs11@gmail.com>
2015-12-30 16:58:05 -0500

    Initial commit with dummy data and readme summary

---
+++ js/functions.js
@@ -0,0 +1,148 @@
+/* General number functions */
+
+// update symbol within span object
+function changeSymbol(span, symbol)
+{
+    if (span) {
+        span.innerHTML = symbol;
+    }
+}
+// return number from string
+function parseNumber(num)
+{
+    return parseFloat(num.replace(/[^0-9\.\-]+/g,""));
+}
+// display number as currency
+function readableCurrency(num)
+{
+    return "$" + String(num).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+}
+
+
+
+/* Account nesting functions */
+// On click, expand/close nested accounts, change totals in account and update graph values
+$(".account").on("click", function(e) {
+    if (!$(this).attr('children')) {
+        // If the account does not have any nested accounts, do nothing
+        return false;
+    }
+    // return list of child accounts
+    /* children = ['acct1', 'acct2'] */
+    var children = JSON.parse($(this).attr('children'));
+    var selector = "";
+    // check if expanding or closing nested accounts
+    var open = $(this).attr('open');
+    values = {};
+    $.each(children, function (k, v) {
+        selector += "#" + v + ", ";
+        array = [];
+        $.each($('#'+v).children('td'), function(key, value) {
+            array.push(parseNumber(value.innerHTML));
+        });
+        v = v + "|" + $('#'+v).attr('name');
+        values[v] = array;
+    });
+    // create array of values
+    /* value = {
+        acct1: [1, 2, 3],
+        acct2: [4, 5, 6]
+    } */
+    selector = selector.substr(0, selector.length-2);
+    /* selector = '#acct1, acct2, acct3' */
+
+    /* Change value based on all values in children accounts.
+    The change values only matter to the account rows, not the graph */
+    $.each($(this).children('td'), function(k, v) {
+        var amount = parseNumber(v.innerHTML);
+        $.each(values, function (key, value) {
+            if (open) {
+                amount += value[k];
+            } else {
+                amount -= value[k];
+            }
+        });
+        // change the values displayed in the html
+        v.innerHTML = readableCurrency(amount);
+    });
+
+    // open or close effects (procedural)
+    if (open) {
+        changeSymbol($(this).find('th').find('span')[0], '&#9899');
+        $(selector).hide();
+        $(this).attr('open', false);
+        updateChart(values, false);
+    } else {
+        changeSymbol($(this).find('th').find('span')[0], '&#9898');
+        $(selector).show();
+        $(this).attr('open', true);
+        updateChart(values, true);
+    }
+    return;
+});
+
+
+
+/* Percent functions */
+// display percent box when expenses are hovered over
+$(".range").hover(function () {
+    setTimeout(function(){
+        $("#percents").show();
+    }, 500);
+    $("#" + $(this).attr('acctid') + $(this).attr('colid')).show();
+}, function() {
+    $("#" + $(this).attr('acctid') + $(this).attr('colid')).hide();
+});
+// minimize percent box
+$( "#percent_hide" ).on("click", function() {
+    $("#percents").hide();
+});
+
+// add calendar symbol and selector to inputs
+$( ".datepicker" ).datepicker({
+    showOn: "button",
+    buttonImage: "img/calendar.gif",
+    buttonImageOnly: true,
+    buttonText: "select date",
+    dateFormat: 'yy-mm-dd'
+});
+
+
+
+/* Chart functions */
+// Global options for chart
+var options = {
+    multiTooltipTemplate: "<%=datasetLabel%>: <%= value %>",
+}
+var ctx = $("#line_graph").get(0).getContext("2d");
+// update values for the chart
+function updateChart(values, add)
+{
+    if (add) {
+        // add values of children accounts
+        $.each(values, function (key, value) {
+            key = key.split('|');
+            if (!(data['datasets'][key[0]])) {
+                data['datasets'][key[0]] = {
+                    label: key[1],
+                    fillColor: "rgba(255,255,255,0)",
+                    strokeColor: "rgba(0,0,0,1)",
+                    pointColor: "rgba(0,0,0,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(0,0,0,1)",
+                    data: value
+                };
+            }
+        });
+    } else {
+        // remove values of children accounts
+        $.each(values, function (key, value) {
+            key = key.split('|');
+            delete (data['datasets'][key[0]]);
+        });
+    }
+    new Chart(ctx).Line(data, options);
+}
+// display chart after page loads
+new Chart(ctx).Line(data, options);
---
+++ readme.md
@@ -0,0 +1,16 @@
+This is an example Profit and Loss Report for the finance/accounting application.
+The backend, which supplies the data, is proprietary and cannot be hosted on github.
+
+The dummy data is included so that the user can experience what the basic functionality is.
+In this application, the data is provided from a report object built from MySql and Php.
+The inputs for the backend include a user interface for entering journal entries and
+an API to collect sales and transaction data from various sources, such as banks, credit cards,
+online marketplaces and e-commerce platforms.
+
+Report features:
+Parent expenses can drill-down for further details. (click)
+Expenses display a percentage of revenue and percent of total expenses. (hover)
+Report date ranges can be set to compare time periods (not available without backend processing)
+
+If you are interested in purchasing a license to use this application,
+please contact the Cresca Group.
